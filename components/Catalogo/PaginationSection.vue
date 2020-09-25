1493a95769d6aacf3efbf857cf2db106418c69a8
Walter Renzo Muñoz Arellano <wmunozarellano@gmail.com>
2020-09-25 10:41:24 -0500

    [UPDATE] Filter Page, Adding Pagination

--- components/Catalogo/PaginationSection.vue
+++ components/Catalogo/PaginationSection.vue
@@ -15,29 +15,70 @@
             </button>
       </b-row>
       <b-row class="rowModelos">
-
-      </b-row>
-      <b-row>
-          <h6>paginacion</h6>
+          <AutoItem :car="auto" v-for="(auto,index) in autosFilter" :key="index"/>
+          <b-pagination-nav
+            v-if="pages !== null"
+            :link-gen="linkGen"
+            :value="currentPage"
+            :number-of-pages="pages" use-router
+            :prefetch="true"
+            class="paginationStyle"
+        ></b-pagination-nav>
       </b-row>
   </b-container>
 </template>
 
 <script>
+import AutoItem from '~/components/General/AutoItem.vue'
 export default {
     data(){
         return{
-            autos: []
+            autos: [],
+            autosFilter: [],
+            perPage: 8,
+            currentPage: null,
+            pages: null,
+            brandFilter:[],
+            priceFilter: [],
         }
     },
+    components:{
+        AutoItem,
+    },
+    params:{
+        modelo: String,
+        page: Number,
+    },
+     computed: {
+      rows() {
+        return this.autos.length
+      }
+    },
    async mounted(){
+       await this.$store.dispatch('autos/getAllCars');
+       if(this.$route.params.modelo && this.$route.params.page){
+           this.brandFilter.push(this.$route.params.modelo)
+           this.currentPage = this.$route.params.page;
+       }else{
+           this.$route.push({name: 'index'})
+       }
        await this.getAutos()
+        
     },
     methods:{
        async getAutos () {
-         this.autos = await this.$store.state.autos.autosSuzuki;
+         this.autos = await this.$store.getters['autos/getAllBrandCars'];
          console.log("getAutos -> this.autos", this.autos)
-       }
+         this.pages = this.autos.length % this.perPage === 0 ? 
+                        Math.floor(this.autos.length/this.perPage) :
+                        Math.floor(this.autos.length/this.perPage) + 1
+         this.autosFilter = this.autos.slice(
+                this.perPage * (this.currentPage - 1),
+                this.perPage * this.currentPage)
+       },
+       linkGen(pageNum) {
+            return `/catalogo/${this.$route.params.modelo}/${pageNum}`
+       },
     }
 }
 </script>
--- components/Home/SectionButton.vue
+++ components/Home/SectionButton.vue
@@ -2,9 +2,9 @@
   <b-container class="section3Home">
       <b-row>
           <div class="col-12">
-              <a href="/catalogo">
+              <nuxt-link to="/catalogo/todos/1">
                 Ver todos los <span>modelos</span>
-            </a>
+              </nuxt-link>
           </div>
       </b-row>
   </b-container>
